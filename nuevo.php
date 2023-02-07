a8df1171c1927e84a8919f49f990250346072b94
Jorge MC <jorgemartincasasola@gmail.com>
2023-02-07 23:33:37 +0100

    Create nuevo.php

---
+++ nuevo.php
@@ -0,0 +1,112 @@
+<section class="elementor-section elementor-top-section elementor-element elementor-element-9390aba elementor-section-content-top elementor-hidden-tablet elementor-hidden-mobile elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="9390aba" data-element_type="section" style="">
+<div class="elementor-container elementor-column-gap-default">
+<div class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-0f10fbc" data-id="0f10fbc" data-element_type="column" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+<div class="elementor-widget-wrap elementor-element-populated">
+<div class="elementor-element elementor-element-75362b2 anwp-pg-post-teaser__post-icon--position-bottom-right anwp-grid_card_bg_effect-darken_only anwp-pg-post-teaser__post-icon--size-16 elementor-widget elementor-widget-anwp-pg-hero-block" data-id="75362b2" data-element_type="widget" data-widget_type="anwp-pg-hero-block.default">
+<div class="elementor-widget-container">
+<div class="anwp-pg-wrap">
+<div class="d-flex anwp-row flex-wrap no-gutters anwp-pg-hero-block anwp-pg-posts-wrapper anwp-pg-gutter--thin">
+<div class="anwp-col-sm-9 anwp-col-lg-6 anwp-pg-height-2x anwp-pg-hero-main-block-wrapper">
+<div class="anwp-pg-post-teaser anwp-pg-post-teaser--layout-c anwp-pg-hero-main-block my-0" style="">
+<div class="anwp-pg-post-teaser__thumbnail position-relative">
+<img loading="lazy" class="anwp-pg-post-teaser__thumbnail-img d-block anwp-pg-height-180 anwp-object-cover m-0 w-100 lazyloaded" alt="Las 10 mejores churrerías de Madrid" src="https://2022.unbuendiaenmadrid.com/wp-content/uploads/2022/11/Chocolateria-1902-785x981.jpg.webp" data-src="https://2022.unbuendiaenmadrid.com/wp-content/uploads/2022/11/Chocolateria-1902-785x981.jpg.webp" decoding="async" data-src-webp="https://2022.unbuendiaenmadrid.com/wp-content/uploads/2022/11/Chocolateria-1902-785x981.jpg.webp"><noscript><img loading="lazy" class="anwp-pg-post-teaser__thumbnail-img d-block anwp-pg-height-180 anwp-object-cover m-0 w-100"
+				alt="Las 10 mejores churrerías de Madrid"
+				src="https://2022.unbuendiaenmadrid.com/wp-content/uploads/2022/11/Chocolateria-1902-785x981.jpg" data-eio="l" /></noscript>
+<div class="anwp-pg-post-teaser__muted_bg"></div>
+<div class="anwp-pg-post-teaser__thumbnail-bg anwp-position-cover"></div>
+<div class="anwp-pg-post-teaser__content d-flex flex-column anwp-position-cover">
+<div class="anwp-pg-post-teaser__top-meta d-flex mb-2">
+</div>
+<div class="anwp-pg-post-teaser__bottom-block d-flex align-items-center position-relative py-4 px-1 mb-3 mt-auto">
+<div class="anwp-pg-post-teaser__title anwp-font-heading">
+Las 10 mejores churrerías de Madrid </div>
+</div>
+</div>
+<a class="anwp-position-cover anwp-link-without-effects" href="https://2022.unbuendiaenmadrid.com/gastronomia/cafes/las-10-mejores-churrerias-de-madrid/" aria-hidden="true" style=""></a>
+</div>
+</div>
+</div>
+<div class="anwp-col-sm-3 anwp-pg-hero-secondary-block-wrapper">
+<div class="anwp-pg-post-teaser anwp-pg-post-teaser--layout-c anwp-pg-hero-secondary-block" style="">
+<div class="anwp-pg-post-teaser__thumbnail position-relative">
+<img loading="lazy" class="anwp-pg-post-teaser__thumbnail-img d-block anwp-pg-height-180 anwp-object-cover m-0 w-100 lazyloaded" alt="Los mejores cocidos de Madrid" src="https://2022.unbuendiaenmadrid.com/wp-content/uploads/2020/02/Cocido-de-La-Clave.jpg" data-src="https://2022.unbuendiaenmadrid.com/wp-content/uploads/2020/02/Cocido-de-La-Clave.jpg" decoding="async"><noscript><img loading="lazy" class="anwp-pg-post-teaser__thumbnail-img d-block anwp-pg-height-180 anwp-object-cover m-0 w-100"
+				alt="Los mejores cocidos de Madrid"
+				src="https://2022.unbuendiaenmadrid.com/wp-content/uploads/2020/02/Cocido-de-La-Clave.jpg" data-eio="l" /></noscript>
+<div class="anwp-pg-post-teaser__muted_bg"></div>
+<div class="anwp-pg-post-teaser__thumbnail-bg anwp-position-cover"></div>
+<div class="anwp-pg-post-teaser__content d-flex flex-column anwp-position-cover">
+<div class="anwp-pg-post-teaser__top-meta d-flex mb-2">
+</div>
+<div class="anwp-pg-post-teaser__bottom-block d-flex align-items-center position-relative py-4 px-1 mb-3 mt-auto">
+<div class="anwp-pg-post-teaser__title anwp-font-heading">
+Los mejores cocidos de Madrid </div>
+</div>
+</div>
+<a class="anwp-position-cover anwp-link-without-effects" href="https://2022.unbuendiaenmadrid.com/gastronomia/restaurantes/restaurantes-de-madrid/los-mejores-cocidos-de-madrid/" aria-hidden="true" style=""></a>
+</div>
+</div>
+<div class="anwp-pg-post-teaser anwp-pg-post-teaser--layout-c anwp-pg-hero-secondary-block">
+<div class="anwp-pg-post-teaser__thumbnail position-relative">
+<img loading="lazy" class="anwp-pg-post-teaser__thumbnail-img d-block anwp-pg-height-180 anwp-object-cover m-0 w-100 lazyloaded" alt="Las mejores librerías de Madrid" src="https://2022.unbuendiaenmadrid.com/wp-content/uploads/2021/11/libreria-bardon-785x589.jpg.webp" data-src="https://2022.unbuendiaenmadrid.com/wp-content/uploads/2021/11/libreria-bardon-785x589.jpg.webp" decoding="async" data-src-webp="https://2022.unbuendiaenmadrid.com/wp-content/uploads/2021/11/libreria-bardon-785x589.jpg.webp"><noscript><img loading="lazy" class="anwp-pg-post-teaser__thumbnail-img d-block anwp-pg-height-180 anwp-object-cover m-0 w-100"
+				alt="Las mejores librerías de Madrid"
+				src="https://2022.unbuendiaenmadrid.com/wp-content/uploads/2021/11/libreria-bardon-785x589.jpg" data-eio="l" /></noscript>
+<div class="anwp-pg-post-teaser__muted_bg"></div>
+<div class="anwp-pg-post-teaser__thumbnail-bg anwp-position-cover"></div>
+<div class="anwp-pg-post-teaser__content d-flex flex-column anwp-position-cover">
+<div class="anwp-pg-post-teaser__top-meta d-flex mb-2">
+</div>
+<div class="anwp-pg-post-teaser__bottom-block d-flex align-items-center position-relative py-4 px-1 mb-3 mt-auto">
+<div class="anwp-pg-post-teaser__title anwp-font-heading">
+Las mejores librerías de Madrid </div>
+</div>
+</div>
+<a class="anwp-position-cover anwp-link-without-effects" href="https://2022.unbuendiaenmadrid.com/turismo-por-madrid/las-mejores-librerias-de-madrid/" aria-hidden="true"></a>
+</div>
+</div>
+</div>
+<div class="anwp-col-sm-3 anwp-pg-hero-secondary-block-wrapper d-sm-none d-lg-block">
+<div class="anwp-pg-post-teaser anwp-pg-post-teaser--layout-c anwp-pg-hero-secondary-block" style="">
+<div class="anwp-pg-post-teaser__thumbnail position-relative">
+<img loading="lazy" class="anwp-pg-post-teaser__thumbnail-img d-block anwp-pg-height-180 anwp-object-cover m-0 w-100 lazyloaded" alt="Sitios recomendados para comer pizza en Madrid" src="https://2022.unbuendiaenmadrid.com/wp-content/uploads/2021/07/lamucca1-e1643218216559.jpg.webp" data-src="https://2022.unbuendiaenmadrid.com/wp-content/uploads/2021/07/lamucca1-e1643218216559.jpg.webp" decoding="async" data-src-webp="https://2022.unbuendiaenmadrid.com/wp-content/uploads/2021/07/lamucca1-e1643218216559.jpg.webp"><noscript><img loading="lazy" class="anwp-pg-post-teaser__thumbnail-img d-block anwp-pg-height-180 anwp-object-cover m-0 w-100"
+				alt="Sitios recomendados para comer pizza en Madrid"
+				src="https://2022.unbuendiaenmadrid.com/wp-content/uploads/2021/07/lamucca1-e1643218216559.jpg" data-eio="l" /></noscript>
+<div class="anwp-pg-post-teaser__muted_bg"></div>
+<div class="anwp-pg-post-teaser__thumbnail-bg anwp-position-cover"></div>
+<div class="anwp-pg-post-teaser__content d-flex flex-column anwp-position-cover">
+<div class="anwp-pg-post-teaser__top-meta d-flex mb-2">
+</div>
+<div class="anwp-pg-post-teaser__bottom-block d-flex align-items-center position-relative py-4 px-1 mb-3 mt-auto">
+<div class="anwp-pg-post-teaser__title anwp-font-heading">
+Sitios recomendados para comer pizza en Madrid </div>
+</div>
+</div>
+<a class="anwp-position-cover anwp-link-without-effects" href="https://2022.unbuendiaenmadrid.com/especiales/mejores-pizzas-madrid/" aria-hidden="true" style=""></a>
+</div>
+</div>
+<div class="anwp-pg-post-teaser anwp-pg-post-teaser--layout-c anwp-pg-hero-secondary-block">
+<div class="anwp-pg-post-teaser__thumbnail position-relative">
+<img loading="lazy" class="anwp-pg-post-teaser__thumbnail-img d-block anwp-pg-height-180 anwp-object-cover m-0 w-100 lazyloaded" alt="Magia, Monólogos y Bavarian Christmas en las Mágicas Navidades" src="https://2022.unbuendiaenmadrid.com/wp-content/uploads/2018/12/IMG-20191205-WA0001.jpg.webp" data-src="https://2022.unbuendiaenmadrid.com/wp-content/uploads/2018/12/IMG-20191205-WA0001.jpg.webp" decoding="async" data-src-webp="https://2022.unbuendiaenmadrid.com/wp-content/uploads/2018/12/IMG-20191205-WA0001.jpg.webp"><noscript><img loading="lazy" class="anwp-pg-post-teaser__thumbnail-img d-block anwp-pg-height-180 anwp-object-cover m-0 w-100"
+				alt="Magia, Monólogos y Bavarian Christmas en las Mágicas Navidades"
+				src="https://2022.unbuendiaenmadrid.com/wp-content/uploads/2018/12/IMG-20191205-WA0001.jpg" data-eio="l" /></noscript>
+<div class="anwp-pg-post-teaser__muted_bg"></div>
+<div class="anwp-pg-post-teaser__thumbnail-bg anwp-position-cover"></div>
+<div class="anwp-pg-post-teaser__content d-flex flex-column anwp-position-cover">
+<div class="anwp-pg-post-teaser__top-meta d-flex mb-2">
+</div>
+<div class="anwp-pg-post-teaser__bottom-block d-flex align-items-center position-relative py-4 px-1 mb-3 mt-auto">
+<div class="anwp-pg-post-teaser__title anwp-font-heading">
+Magia, Monólogos y Bavarian Christmas en las Mágicas Navidades </div>
+</div>
+</div>
+<a class="anwp-position-cover anwp-link-without-effects" href="https://2022.unbuendiaenmadrid.com/acontecimientos/bavarian-christmas-en-torrejon-de-ardoz/" aria-hidden="true"></a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
